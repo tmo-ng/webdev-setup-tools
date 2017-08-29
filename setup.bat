ce2937ec46ced76dccd45f9e7b7d31baceaedf81
DeJarlais, Christopher <Christopher.DeJarlais1@T-Mobile.com>
2017-08-29 13:48:33 -0700

    Add setup.bat and setup.sh for installing node on windows and osx/linux respectively

---
+++ setup.bat
@@ -0,0 +1,70 @@
+@echo OFF
+setlocal enabledelayedexpansion
+set userNodeVersion=""
+set latestNodeVersion=""
+set downloadsFolder=%userprofile%\Downloads\
+set npmCmd=npm
+set powershellVersion=""
+for /f "tokens=*" %%i in ('powershell.exe -command "$output = Get-Host | Select-Object Version; $output -match '[3-9].0'"') do (
+  set powershellVersion=%%i
+)
+if !powershellVersion! == False (
+    set /p missingPrompt=This program requires powershell version 3.0 or higher.
+    set /p missingPrompt=This can be downloaded at https://www.microsoft.com/en-us/download/details.aspx?id=34595
+    exit /b 0
+)
+set /p missingPrompt=This program must be run with administrative privileges.
+for /f "tokens=*" %%i in ('node -v 2^>nul') do (
+  set userNodeVersion=%%i
+)
+for /f "tokens=*" %%i in ('powershell -command "& { . .\nodeInstallerScript.ps1; FindMostRecentVersion }"') do (
+  set latestNodeVersion=%%i
+)
+if !userNodeVersion! == "" (
+  call :InstallNode
+  exit /b 0
+)
+call :ValidateNode
+exit /b 0
+
+:CheckNodeCompatibility
+set notCompatible=""
+set nodePath=""
+for /f "tokens=*" %%i in ('node -e "console.log(require('semver').outside('!userNodeVersion:~1!', require('./package.json').globals.engines.node, '<'))"') do (
+  set notCompatible=%%i
+)
+for /f "tokens=*" %%i in ('where node.exe') do (
+  set nodePath=%%i
+)
+if !notCompatible! == true (
+    echo local node version !userNodeVersion! is out of date, updating now
+    powershell.exe -command "$client = New-Object System.Net.WebClient;$client.Headers['User-Agent'] = 'myUser';$client.DownloadFile('https://nodejs.org/download/release/latest/win-x64/node.exe', '!nodePath!')"
+    set userNodeVersion=!latestNodeVersion!
+)
+if NOT !userNodeVersion! == !latestNodeVersion! (
+    set /p optionalPrompt=a newer version of node is available. would you like to install this version now ^(yes/no^)^?
+    if NOT !optionalPrompt! == no (
+        powershell.exe -command "$client = New-Object System.Net.WebClient;$client.Headers['User-Agent'] = 'myUser';$client.DownloadFile('https://nodejs.org/download/release/latest/win-x64/node.exe', '!nodePath!')"
+        set userNodeVersion=!latestNodeVersion!
+    )
+)
+echo user node version !userNodeVersion! is up to date
+call :StartNodeScript
+exit /b 0
+
+:InstallNode
+set /p missingPrompt=node is missing, press any key to start installation
+powershell.exe -command "$client = New-Object System.Net.WebClient;$client.Headers['User-Agent'] = 'myUser';$client.DownloadFile('https://nodejs.org/dist/latest/node-!latestNodeVersion!-x64.msi', '!downloadsFolder!node-!latestNodeVersion!-x64.msi')"
+msiexec /qn /l* C:\node-log.txt /i !downloadsFolder!node-!latestNodeVersion!-x64.msi
+set userNodeVersion=!latestNodeVersion!
+echo node was installed with version !latestNodeVersion!
+cd ../ && powershell.exe -command "$env:Path = [System.Environment]::GetEnvironmentVariable('Path', 'Machine'); npm install --scripts-prepend-node-path=true;" && call :StartNodeScript
+exit /b 0
+
+:ValidateNode
+cd ../ && npm install --scripts-prepend-node-path=true && call :CheckNodeCompatibility
+exit /b 0
+
+:StartNodeScript
+powershell.exe -command "$env:Path = [System.Environment]::GetEnvironmentVariable('Path', 'Machine'); node -e \"require('./setup.js').installEverything()\";"
+exit /b 0
---
+++ setup.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+#load nvm into current environment
+#node -e "console.log(require('semver').outside($localVersion, require('../package.json').globals.engines.node, '<'))"
+load_nvm_script () {
+  #this assumes the recommended installation directory of ~/.nvm
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+}
+get_local_node_version () {
+    currentVersion=$(nvm current | grep -o -E '[0-9]+(\.[0-9]+){1,}' 2> null)
+    echo "$currentVersion"
+}
+get_latest_node_version () {
+  latestVersion=$(nvm ls-remote | grep -o -E '[0-9]+(\.[0-9]+){1,}' | tail -1)
+  echo "$latestVersion"
+}
+#install and use node version
+install_node_version () {
+    #install latest version, and migrate npm to new version
+    latestVersion=$(get_latest_node_version)
+    if nvm install $latestVersion
+    then
+        nvm alias default $latestVersion
+        nvm use $latestVersion
+    else
+        echo "install failed for node"
+    fi
+}
+perform_optional_update () {
+    localVersion=$1
+    latestVersion=$2
+    if [[ $localVersion != $latestVersion ]]; then
+      echo -n "would you like to update node now(y/n)? "
+      read response
+      if [[ $response != "n" ]]; then
+        echo "updating node now.."
+        nvm install $latestVersion
+        nvm alias default $latestVersion
+        nvm use $latestVersion
+      else
+        echo "ignoring node update"
+      fi
+    else
+      echo "local node version $localVersion is up to date"
+    fi
+}
+local_is_not_compatible () {
+  localVersion=$1
+  isCompatible=$(node -e "console.log(require('semver').outside('$localVersion', require('./package.json').globals.engines.node, '<'))")
+  echo "$isCompatible"
+}
+#install dependencies required by setup.js
+install_package_dependencies () {
+    if cd ../; then
+        npm install
+    fi
+}
+run_full_nvm_install () {
+    if curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+    then
+        load_nvm_script
+        install_node_version
+        install_package_dependencies
+    else
+        echo "could not download nvm"
+    fi
+}
+
+main () {
+  if [[ ! -e ~/.bash_profile ]]; then
+    > ~/.bash_profile
+  fi
+  if [[ ! -e ~/.bashrc ]]; then
+    > ~/.bashrc
+  fi
+  load_nvm_script
+  LOCAL_VERSION=$(get_local_node_version)
+  if [ -z "$LOCAL_VERSION" ]; then
+      echo "no version of nvm detected detected, installing now"
+      run_full_nvm_install
+  else
+      echo "now installing required package dependencies"
+      install_package_dependencies
+      #check here for compatibility
+      echo "you are currently using node version $LOCAL_VERSION"
+      if [[ $(local_is_not_compatible $LOCAL_VERSION) == "true" ]]; then
+        echo "local version of node is out of date, updating now."
+        install_node_version
+        nvm reinstall-packages $LOCAL_VERSION
+      else
+        REMOTE_VERSION=$(get_latest_node_version)
+        echo "the latest version available is $REMOTE_VERSION"
+        perform_optional_update $LOCAL_VERSION $REMOTE_VERSION
+      fi
+  fi
+  echo "beginning full install"
+  bash -l -c "node -e \"require('./setup.js').installEverything()\""
+}
+main
