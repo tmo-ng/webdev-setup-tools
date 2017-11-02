e2edc48baef8df4999ece1b210d4f3a07dc6b4e3
Christopher DeJarlais <Christopher.DeJarlais1@T-Mobile.com>
2017-11-01 20:13:10 -0700

    Improve setup.bat error handling

--- setup-scripts/setup.bat
+++ setup-scripts/setup.bat
@@ -6,20 +6,20 @@ set downloadsFolder=%userprofile%\Downloads\
 set npmCmd=npm
 set hasMinPsVersion=""
 set hasScriptExecEnabled=""
+for /f "tokens=*" %%i in ('powershell -command "$policy = Get-ExecutionPolicy; $policy -ne \"restricted\";"') do (
+    set hasScriptExecEnabled=%%i
+)
+if !hasScriptExecEnabled! == False (
+    set /p missingPrompt=This program requires powershell script execution to be enabled.
+    set /p missingPrompt=log into powershell and enter the command "Set-ExecutionPolicy unrestricted", then restart this script.
+    exit /b 0
+)
 for /f "tokens=*" %%i in ('powershell -command "& { . .\nodeInstallerScript.ps1; IsPowershellVersionCompatible }"') do (
     set hasMinPsVersion=%%i
 )
 if !hasMinPsVersion! == False (
     set /p missingPrompt=This program requires powershell version 3.0 or higher.
     set /p missingPrompt=This can be downloaded at https://www.microsoft.com/en-us/download/details.aspx?id=34595
-    exit /b 0
-)
-for /f "tokens=*" %%i in ('powershell -command "& { . .\nodeInstallerScript.ps1; IsScriptExecutionEnabled }"') do (
-    set hasScriptExecEnabled=%%i
-)
-if !hasScriptExecEnabled! == False (
-    set /p missingPrompt=This program requires powershell script execution to be enabled.
-    set /p missingPrompt=See the README in this folder for instructions on setting your policy.
     exit /b 0
 )
 for /f "tokens=*" %%i in ('node -v 2^>nul') do (
@@ -46,15 +46,16 @@ for /f "tokens=*" %%i in ('node -e "console.log(require('semver').outside('!user
 for /f "tokens=*" %%i in ('where node.exe') do (
     set nodePath=%%i
 )
+echo !nodePath!
 if !notCompatible! == true (
     echo local node version !userNodeVersion! is out of date, updating now
-    powershell.exe -command "$client = New-Object System.Net.WebClient;$client.Headers['User-Agent'] = 'myUser';$client.DownloadFile('https://nodejs.org/download/release/latest/win-x64/node.exe', '!nodePath!')"
+    powershell.exe -command "$client = New-Object System.Net.WebClient;$client.Headers['User-Agent'] = 'tmoNg';$client.DownloadFile('https://nodejs.org/dist/latest/win-x64/node.exe', '!nodePath!')"
     set userNodeVersion=!latestNodeVersion!
 )
 if NOT !userNodeVersion! == !latestNodeVersion! (
     set /p optionalPrompt=a newer version of node is available. would you like to install this version now ^(yes/no^)^?
     if NOT !optionalPrompt! == no (
-        powershell.exe -command "$client = New-Object System.Net.WebClient;$client.Headers['User-Agent'] = 'myUser';$client.DownloadFile('https://nodejs.org/download/release/latest/win-x64/node.exe', '!nodePath!')"
+        powershell.exe -command "$client = New-Object System.Net.WebClient;$client.Headers['User-Agent'] = 'tmoNg';$client.DownloadFile('https://nodejs.org/dist/latest/win-x64/node.exe', '!nodePath!')"
         set userNodeVersion=!latestNodeVersion!
     )
 )
@@ -64,7 +65,7 @@ exit /b 0
 
 :InstallNode
 set /p missingPrompt=node is missing, press any key to start installation
-powershell.exe -command "$client = New-Object System.Net.WebClient;$client.Headers['User-Agent'] = 'myUser';$client.DownloadFile('https://nodejs.org/dist/latest/node-!latestNodeVersion!-x64.msi', '!downloadsFolder!node-!latestNodeVersion!-x64.msi')"
+powershell.exe -command "$client = New-Object System.Net.WebClient;$client.Headers['User-Agent'] = 'tmoNg';$client.DownloadFile('https://nodejs.org/dist/latest/node-!latestNodeVersion!-x64.msi', '!downloadsFolder!node-!latestNodeVersion!-x64.msi')"
 msiexec /qn /l* C:\node-log.txt /i !downloadsFolder!node-!latestNodeVersion!-x64.msi
 set userNodeVersion=!latestNodeVersion!
 echo node was installed with version !latestNodeVersion!
